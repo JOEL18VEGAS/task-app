42f687df371d751a84e197fdb81751afb529283b
JOEL18VEGAS <joel18gonzalez@outlook.com>
2023-06-03 16:09:42 -0500

    App finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 import * as React from "react";
 import { useState } from "react";
+import styled, { css } from "styled-components";
+
+// Images
 import Logo from "../images/logo.png";
 import Cancel from "../images/cancel.png";
+import CancelDisable from "../images/cancel-disable.png";
 import Check from "../images/check.png";
+import CheckDisable from "../images/check-disable.png";
 import Delete from "../images/delete.png";
-import styled from "styled-components";
+import AddIcon from "../images/add-icon.png";
+import AddSolidIcon from "../images/add-icon-solid.png";
+import Github from "../images/github-icon.svg";
 
 // ! Page Styles Start
 
+// Main Page Styles
 const pageStyles = {
   display: "flex",
   flexDirection: "column",
@@ -18,20 +26,10 @@ const pageStyles = {
   minHeight: "90vh",
 };
 
-const paragraphStyles = {
-  marginBottom: 48,
-};
-
-const InputComponent = styled.input`
-  /* Global */
-  padding: 1%;
-  margin: 1%;
-  border: none;
-  border-radius: 5px;
-  outline: none;
-
+// Logo Resposive Styles
+const LogoComponent = styled.img`
   /* Desktop */
-  width: 700px;
+  width: 600px;
 
   /* Phone */
   @media (max-width: 480px) {
@@ -41,28 +39,145 @@ const InputComponent = styled.input`
   /* Tablet */
   @media (min-width: 481px) and (max-width: 768px) {
     width: 500px;
+  }
+`;
+
+// Paragraph Resposive Styles
+const Paragraph = styled.p`
+  /* Desktop */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 600px;
+  font-size: 1.2rem;
+  text-align: center;
+
+  /* Phone */
+  @media (max-width: 480px) {
+    width: 300px;
+    font-size: 0.7rem;
+  }
+
+  /* Tablet */
+  @media (min-width: 481px) and (max-width: 768px) {
+    width: 500px;
+    font-size: 1.1rem;
+  }
+`;
+
+// Github Brand
+const GithubBrand = styled.a`
+  font-family: "Quicksand", sans-serif;
+  /* Global */
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  font-size: 0.9rem;
+  margin-left: 1rem;
+  padding: 0.5rem;
+  background-color: #161616;
+  border-radius: 8px;
+  color: white;
+  text-decoration: none;
+
+  width: 140px;
+`;
+
+// Form Container Styles
+const FormContainerStyles = styled.div`
+  /* Global */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  /* Desktop */
+  height: 8vh;
+`;
+
+// Input Resposive Styles
+const InputComponent = styled.input`
+  /* Global */
+  padding: 1rem;
+  border: 2.5px solid #a8a8a8;
+  border-radius: 20px;
+  outline: none;
+  font-size: 1rem;
+  box-shadow: 2px 2px 5px 2px #0000001a;
+
+  /* Desktop */
+  width: 400px;
+  height: 20px;
+
+  /* Phone */
+  @media (max-width: 480px) {
+    width: 200px;
+    height: 12px;
+  }
+
+  /* Tablet */
+  @media (min-width: 481px) and (max-width: 768px) {
+    width: 300px;
+    height: 15px;
   }
 
   /* Focus */
   :focus {
-    box-shadow: 0 0 0 2px #ff23;
+    border: 2.5px solid #902bcf;
   }
 
   /* Placeholder */
   ::placeholder {
-    color: #ff23;
+    color: #a8a8a8;
+    font-size: 1rem;
   }
-
-  /* Text */
-  color: #ff23;
-
-
 `;
 
-// Logo Resposive Styles
-const LogoComponent = styled.img`
+// AddButton Resposive Styles
+const AddButton = styled.button`
+  /* Global */
+  margin: 0.5rem;
+  border: none;
+  outline: none;
+  font-size: 1rem;
+  cursor: pointer;
+  background-color: transparent;
+  background-image: url(${AddIcon});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%;
+
+  :hover {
+    background-image: url(${AddSolidIcon});
+    transition: 0.5s;
+  }
+
   /* Desktop */
-  width: 700px;
+  width: 50px;
+  height: 50px;
+
+  /* Phone */
+  @media (max-width: 480px) {
+    width: 40px;
+    height: 40px;
+  }
+
+  /* Tablet */
+  @media (min-width: 481px) and (max-width: 768px) {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+// Filters Container Styles
+const FiltersContainer = styled.div`
+  /* Global */
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 0.5rem 0;
+
+  /* Desktop */
+  width: 500px;
 
   /* Phone */
   @media (max-width: 480px) {
@@ -71,20 +186,61 @@ const LogoComponent = styled.img`
 
   /* Tablet */
   @media (min-width: 481px) and (max-width: 768px) {
-    width: 500px;
+    width: 400px;
   }
 `;
 
+// FilterButton Resposive Styles
+const FilterButton = styled.button`
+  /* Global */
+  margin: 0.5rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  outline: none;
+  font-size: 1rem;
+  cursor: pointer;
+  border-radius: 20px;
+  background-color: #f5f5f5;
+  color: #902bcf;
+  box-shadow: 2px 2px 5px 2px #0000001a;
+
+  /* focus */
+  :focus {
+    background-color: #902bcf;
+    color: #f5f5f5;
+    transition: 0.5s;
+  }
+
+  /* Desktop */
+  width: 140px;
+  height: 40px;
+
+  /* Phone */
+  @media (max-width: 480px) {
+    width: 100px;
+    height: 30px;
+    font-size: 0.8rem;
+  }
+
+  /* Tablet */
+  @media (min-width: 481px) and (max-width: 768px) {
+    width: 120px;
+    height: 35px;
+    font-size: 0.9rem;
+  }
+`;
+
+// Tasks List Resposive Styles
 const TasksList = styled.ul`
   /* Global */
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 2% 0;
   list-style: none;
-  background-color: #ff23;
-  height: 40vh;
-  overflow-y: scroll;
+  height: 35vh;
+  overflow-y: auto;
 
   /* Hide ScrollBar */
   ::-webkit-scrollbar {
@@ -105,11 +261,156 @@ const TasksList = styled.ul`
   }
 `;
 
-const Task = styled.li`
+// Task Container Resposive Styles
+const TaskContainer = styled.div`
+  /* Global */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0.5rem 0;
 
+  /* Desktop */
+  width: 70%;
+`;
+
+// Task Resposive Styles
+const Task = styled.li`
+  /* Global */
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  /* margin: 0.5rem 0; */
+  box-shadow: 2px 2px 5px 2px #0000001a;
+  border-radius: 10px;
+  font-size: 1.2rem;
+  background-color: #ffff;
+
+  /* click in task change border permanently */
+  :active {
+    border: 2px solid #902bcf;
+  }
+
+  :focus {
+    border: 2px solid #902bcf;
+  }
+
+  /* Desktop */
+  width: 100%;
+  height: 80px;
+
+  /* Phone */
+  @media (max-width: 480px) {
+    width: 100%;
+    height: 60px;
+    font-size: 1rem;
+  }
+
+  /* Tablet */
+  @media (min-width: 481px) and (max-width: 768px) {
+    width: 100%;
+    height: 70px;
+    font-size: 1.1rem;
+  }
+`;
+
+// CompleteButton Resposive Styles
+const CompleteButton = styled.a`
+  /* Global */
+  margin: 0 0.5rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  outline: none;
+  font-size: 1rem;
+  background-color: transparent;
+  cursor: pointer;
+  background-image: url(${Check});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 90%;
+
+  /* Validate with js , if complete is true */
+  ${(props) =>
+    props.complete &&
+    css`
+      background-image: url(${CheckDisable});
+    `}
+
+  /* Desktop */
+  width: 40px;
+  height: 70%;
+
+  /* Phone */
+  @media (max-width: 480px) {
+    width: 30px;
+  }
+
+  /* Tablet */
+  @media (min-width: 481px) and (max-width: 768px) {
+    width: 35px;
+  }
+`;
+
+// CancelButton Resposive Styles
+const CancelButton = styled.span`
+  /* Global */
+  margin: 0 0.5rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  outline: none;
+  font-size: 1rem;
+  background-color: transparent;
+  background-image: url(${CancelDisable});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 80%;
+
+  /* Validate with js , if complete is true */
+  ${(props) =>
+    props.complete &&
+    css`
+      background-image: url(${Cancel});
+    `}
+
+  /* Desktop */
+  width: 40px;
+  height: 70%;
+
+  /* Phone */
+  @media (max-width: 480px) {
+    width: 30px;
+  }
+
+  /* Tablet */
+  @media (min-width: 481px) and (max-width: 768px) {
+    width: 30px;
+  }
+`;
+
+// RemoveButton Resposive Styles
+const RemoveButton = styled.span`
+  /* Global */
+  cursor: pointer;
+
+  /* Desktop */
+  width: 80px;
+  height: 70%;
+
+  /* Phone */
+  @media (max-width: 480px) {
+    width: 50px;
+  }
+
+  /* Tablet */
+  @media (min-width: 481px) and (max-width: 768px) {
+    width: 60px;
+  }
 `;
 
 // ! Page Styles End
+
+// ! Page Logic Start
 
 const IndexPage = () => {
   // Tasks State
@@ -203,57 +504,59 @@ const IndexPage = () => {
       </div>
 
       {/* Title */}
-      <p style={paragraphStyles}>
-        Applicatión created by:{" "}
-        <a href="https://github.com/JOEL18VEGAS" target={"_blank"}>
-          JOEL VEGAS
-        </a>
-      </p>
+      <Paragraph>
+        Applicatión created by:
+        <GithubBrand href="https://github.com/JOEL18VEGAS" target={"_blank"}>
+          <img src={Github} alt="github" />
+          <span>JOEL VEGAS</span>
+        </GithubBrand>
+      </Paragraph>
 
       {/* Form */}
-      <div>
+      <FormContainerStyles>
         <InputComponent
           onChange={handleInputChange}
           value={task}
           type="text"
-          placeholder="Task"
+          placeholder="Add Task..."
         />
-        <button onClick={() => addTask()}>Add</button>
-      </div>
+        <AddButton disabled={task.length === 0} onClick={() => addTask()} />
+      </FormContainerStyles>
 
       {/* Filters */}
-      <div>
-        <button onClick={() => activeTodos()}>Active</button>
-        <button onClick={() => completedTodos()}>Completed</button>
-        <button onClick={() => allTodos()}>All</button>
-      </div>
+      <FiltersContainer>
+        <FilterButton onClick={() => activeTodos()}>Active</FilterButton>
+        <FilterButton onClick={() => completedTodos()}>Completed</FilterButton>
+        <FilterButton onClick={() => allTodos()}>All</FilterButton>
+      </FiltersContainer>
 
       {/* Task List */}
-      <div>
-        <TasksList>
-          {filter.map((task) => (
-            <li key={task.id}>
-              <button onClick={() => completeTask(task.id)}>
-                <img src={Check} alt="Task App" />
-              </button>
+
+      <TasksList>
+        {filter.map((task) => (
+          <TaskContainer>
+            <Task key={task.id}>
+              <CompleteButton
+                complete={task.completed}
+                onClick={() => completeTask(task.id)}
+              />
               <span>{task.name}</span>
-              <button
+
+              <CancelButton
+                complete={task.completed}
                 disabled={!task.completed}
                 onClick={() => removeTask(task.id)}
-              >
-                <img src={Cancel} alt="Task App" />
-              </button>
-            </li>
-          ))}
-        </TasksList>
-      </div>
+              />
+            </Task>
+          </TaskContainer>
+        ))}
+      </TasksList>
 
       {/* Delete All Completed */}
-      <div>
-        <button onClick={() => clearTasks()}>
-          <img src={Delete} alt="Task App" />
-        </button>
-      </div>
+
+      <RemoveButton onClick={() => clearTasks()}>
+        <img src={Delete} width="100%" alt="Delete All Completed" />
+      </RemoveButton>
 
       {/* Footer */}
       <div>
@@ -267,4 +570,4 @@ const IndexPage = () => {
 
 export default IndexPage;
 
-export const Head = () => <title>Home Page</title>;
+export const Head = () => <title>Task App</title>;
